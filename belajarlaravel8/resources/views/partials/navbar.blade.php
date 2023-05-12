150eff95f2536fe31704dbaf32e8eba366262a15
fajarrachmath <fajar.rachmat.h@gmail.com>
2023-05-12 10:51:09 +0700

    Perubahan Navbar

--- belajarlaravel8/resources/views/partials/navbar.blade.php
+++ belajarlaravel8/resources/views/partials/navbar.blade.php
@@ -2,13 +2,44 @@
     class="navbar navbar-expand navbar-light bg-white fixed-top topbar static-top shadow"
 >
     <div class="container-fluid">
-        <a class="navbar-brand" href="https://rsj.jabarprov.go.id/">
-            <img
-                src="img/rsjjabar.png"
-                alt=""
-                height="35"
-                class="d-inline-block align-text-top"
-            />
-        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+                <li class="nav-item">
+                    <a class="navbar-link" href="https://rsj.jabarprov.go.id/">
+                        <img
+                            src="img/rsjjabar.png"
+                            alt=""
+                            height="50"
+                            class="d-inline-block align-text-top"
+                        />
+                    </a>
+                </li>
+                <div class="vr m-2"></div>
+                <li class="nav-item">
+                    <a
+                        class="navbar-link"
+                        href="https://http://103.147.222.122:92//"
+                    >
+                        <img
+                            src="img/WBS.png"
+                            alt=""
+                            height="50"
+                            class="d-inline-block align-text-top"
+                        />
+                    </a>
+                </li>
+                <div class="vr m-2"></div>
+                <li class="nav-item">
+                    <a class="navbar-link" href="https://lapor.go.id/">
+                        <img
+                            src="img/lapor.png"
+                            alt=""
+                            height="50"
+                            class="d-inline-block align-text-top"
+                        />
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
